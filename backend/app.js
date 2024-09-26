6523bdf3eb131b30b04d0249cf55de9705cda421
Neil Pierre-Louis <59701044+neilpl24@users.noreply.github.com>
2024-09-25 22:50:41 -0400

    Added goalie stats, removed some stats that I need to fix  (#48)
    * Small CSS hover tweaks to LBs
    
    * Added real goalie stats, removed some that I need to fix

--- backend/app.js
+++ backend/app.js
@@ -473,12 +473,108 @@ app.get("/leaderboard/skaters", (req, res, next) => {
   });
 });
 
+app.get("/leaderboard/goalies", (req, res, next) => {
+  let filters = req.query;
+  let { query, params } = getPlayers(filters, "goalie_numbers_no_percentile");
+
+  db.all(query, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    rows = rows.map((row) => ({
+      position: row.position,
+      firstName: row.firstName,
+      lastName: row.lastName,
+      birthDate: row.birthDate,
+      nationality: row.nationality,
+      height: row.height,
+      weight: row.weight,
+      team: row.team,
+      teamId: Number(row.team_id),
+      handedness: row.handedness,
+      toi: row.TOI,
+      starts: row.starts,
+      shootout: row.shootout,
+      lowDanger: row.low_danger * row.TOI,
+      medDanger: row.medium_danger * row.TOI,
+      medDangerFreq: row.medium_danger_freq,
+      highDanger: row.high_danger * row.TOI,
+      highDangerFreq: row.high_danger_freq,
+      gsax:
+        row.low_danger * row.TOI +
+        row.medium_danger * row.TOI +
+        row.high_danger * row.TOI,
+      pk: row.pk * row.TOI,
+      ev: row.ev * row.TOI,
+      shots: row.shots,
+      playerID: Number(row.player_id),
+    }));
+
+    res.status(200).json(rows);
+  });
+});
+
+app.get("/leaderboard/teams", (req, res, next) => {
+  // let { query, params } = getPlayers(filters, "teams");
+  const params = req.params; // others have req.params here, but we don't need them yet
+
+  const query = `SELECT * FROM teams WHERE season = (SELECT MAX(season) FROM teams) LIMIT 32;`;
+
+  teams_db.all(query, [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    rows = rows.map((row) => ({
+      teamName: row.team_name,
+      teamId: Number(row.team_id),
+      season: row.season,
+      wins: row.wins,
+      losses: row.losses,
+      otl: row.otl,
+      gf: row.gf,
+      ga: row.ga,
+      points: row.points,
+      gamesPlayed: row.games_played,
+      division: row.division,
+      ev_xGF: row.ev_xGF,
+      ev_xGFRank: row.ev_xGF_rank,
+      ev_xGA: row.ev_xGA,
+      ev_xGARank: row.ev_xGA_rank,
+      xGF: row.xGF,
+      xGA: row.xGA,
+      l10Wins: row.l10Wins,
+      l10Losses: row.l10Losses,
+      leagueRank: row.leagueRank,
+      divisionRank: row.divisionRank,
+      pp: row.pp,
+      ppRank: row.pp_rank,
+      pk: row.pk,
+      pkRank: row.pk_rank,
+      gfNonEmpty: row.gf_non_empty,
+      gaNonEmpty: row.ga_non_empty,
+      finishing: row.finishing,
+      finishingRank: row.finishing_rank,
+      gsax: row.gsax,
+      gsaxRank: row.gsax_rank,
+      xGPercentage: row.xGPercentage,
+      xGPercentageRank: row.xGPercentage_rank,
+    }));
+
+    res.status(200).json(rows);
+  });
+});
+
 app.get("/scores/:date", async (req, res, next) => {
   const date = req.query.date;
 
   const data = await (
     await fetch(`https://api-web.nhle.com/v1/schedule/${date}`)
   ).json();
+
   if (!data || data.gameWeek[0].numberOfGames == 0) {
     res.status(200).json([]);
     return;
@@ -709,99 +805,6 @@ app.get("/game/:year/:gamePk", (req, res, next) => {
 
 app.get("/scores/:year/:gamepk", async (req, res, next) => {
   const gamepk = req.query.gamepk;
-});
-
-app.get("/leaderboard/goalies", (req, res, next) => {
-  let filters = req.query;
-  let { query, params } = getPlayers(filters, "goalie_numbers");
-
-  db.all(query, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-
-    rows = rows.map((row) => ({
-      position: row.position,
-      firstName: row.firstName,
-      lastName: row.lastName,
-      birthDate: row.birthDate,
-      nationality: row.nationality,
-      height: row.height,
-      weight: row.weight,
-      team: row.team,
-      teamId: Number(row.team_id),
-      handedness: row.handedness,
-      toi: row.TOI,
-      starts: row.starts,
-      shootout: row.shootout,
-      lowDanger: row.low_danger,
-      mediumDanger: row.medium_danger,
-      mediumDangerFreq: row.medium_danger_freq,
-      highDanger: row.high_danger,
-      highDangerFreq: row.high_danger_freq,
-      pk: row.pk,
-      ev: row.ev,
-      shots: row.shots,
-      playerID: Number(row.player_id),
-    }));
-
-    res.status(200).json(rows);
-  });
-});
-
-app.get("/leaderboard/teams", (req, res, next) => {
-  const type = Number(req.query.type);
-  let filters = req.query;
-  // let { query, params } = getPlayers(filters, "teams");
-  const params = ""; // others have req.params here, but we don't need them yet
-
-  const query = `SELECT * FROM teams WHERE season = (SELECT MAX(season) FROM teams) LIMIT 32;`;
-
-  teams_db.all(query, [], (err, rows) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-
-    rows = rows.map((row) => ({
-      teamName: row.team_name,
-      teamId: Number(row.team_id),
-      season: row.season,
-      wins: row.wins,
-      losses: row.losses,
-      otl: row.otl,
-      gf: row.gf,
-      ga: row.ga,
-      points: row.points,
-      gamesPlayed: row.games_played,
-      division: row.division,
-      ev_xGF: row.ev_xGF,
-      ev_xGFRank: row.ev_xGF_rank,
-      ev_xGA: row.ev_xGA,
-      ev_xGARank: row.ev_xGA_rank,
-      xGF: row.xGF,
-      xGA: row.xGA,
-      l10Wins: row.l10Wins,
-      l10Losses: row.l10Losses,
-      leagueRank: row.leagueRank,
-      divisionRank: row.divisionRank,
-      pp: row.pp,
-      ppRank: row.pp_rank,
-      pk: row.pk,
-      pkRank: row.pk_rank,
-      gfNonEmpty: row.gf_non_empty,
-      gaNonEmpty: row.ga_non_empty,
-      finishing: row.finishing,
-      finishingRank: row.finishing_rank,
-      gsax: row.gsax,
-      gsaxRank: row.gsax_rank,
-      xGPercentage: row.xGPercentage,
-      xGPercentageRank: row.xGPercentage_rank,
-    }));
-
-    res.status(200).json(rows);
-  });
 });
 
 app.get("/teams/:id", (req, res, next) => {
